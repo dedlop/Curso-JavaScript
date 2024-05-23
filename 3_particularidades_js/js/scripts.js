2d5529856d23a3841cbbee65fc4b119b2d0501f0
dedlop <104914772+dedlop@users.noreply.github.com>
2024-05-23 11:51:07 -0300

    Backup do curso

---
+++ 3_particularidades_js/js/scripts.js
@@ -0,0 +1,23 @@
+console.log("Primeiro")
+
+console.log("Último")
+
+
+// Declara uma variável
+var a = 1;
+
+a = 'teste';
+
+console.log(a);
+
+// Este é um comentário
+
+/*
+    Este
+    é
+    um 
+    comentário
+    de 
+    múltiplas 
+    linhas
+*/
---
+++ 4_variaveis/js/scripts.js
@@ -0,0 +1,41 @@
+var teste = 1;
+
+var nome = 'João';
+
+console.log(teste)
+
+teste = 'Matheus';
+
+console.log(teste)
+
+console.log(nome)
+
+var $nome = 'ASD';
+var _nome = 'ASD2';
+
+console.log($nome);
+console.log(_nome);
+
+var nome5 = 'ASD3';
+
+console.log(nome5);
+
+var meuPrimeiroNome = 'André'; // camelCase
+
+console.log(meuPrimeiroNome);
+
+var meusobrenome = 'Lopes';
+
+console.log(meusobrenome);
+
+let testando = 1;
+const ola = 2;
+
+console.log(testando);
+console.log(ola);
+
+var meuNome;
+
+meuNome = "Matheus";
+
+console.log(meuNome)
