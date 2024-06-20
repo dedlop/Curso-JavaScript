c670cb03eac755fc1ceffab8ce875c4f6f26d95b
dedlop <104914772+dedlop@users.noreply.github.com>
2024-06-20 11:21:42 -0300

    Manipulando a DOM

---
+++ 34_queryselector/js/scripts.js
@@ -0,0 +1,18 @@
+var itensClasse = document.getElementsByClassName('item');
+
+console.log(itensClasse);
+
+// querySelectorAll
+var itensQuery = document.querySelectorAll('#lista2 li');
+
+console.log(itensQuery);
+
+var itensQuery2 = document.querySelectorAll('#lista .item');
+
+console.log(itensQuery2);
+
+// querySelector
+
+var lista = document.querySelector('#lista');
+
+console.log(lista);
