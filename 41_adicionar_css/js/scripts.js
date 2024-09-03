b54debaf78dd7d1e4c4e7815dbd4d273a3a64fc4
dedlop <104914772+dedlop@users.noreply.github.com>
2024-09-03 11:44:46 -0300

    Manipulando css via JS

---
+++ 41_adicionar_css/js/scripts.js
@@ -0,0 +1,14 @@
+// seleciona o elemento
+var title = document.querySelector("#title");
+
+// adicionar o estilo
+title.style.color = "red";
+
+// backgroud-color
+title.style.backgroundColor = "yellow"; 
+
+// selecionar elemento
+var subtitle = document.querySelector(".subtitle");
+
+// adicionar vários estilos
+subtitle.style.cssText = "color: blue; background-color: pink; font-size: 50px"
